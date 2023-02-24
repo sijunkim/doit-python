c9e1cecf63b30c29fd4fee6a9c7e3d01e632917c
kimsijun <papaya9349@naver.com>
2023-02-25 00:37:13 +0900

    black 적용

--- 03/P11660.py
+++ 03/P11660.py
@@ -1,4 +1,4 @@
-'''
+"""
 4 3
 1 2 3 4
 2 3 4 5
@@ -15,9 +15,10 @@
 1 2 1 2 => 2
 2 1 2 1 => 3
 2 2 2 2 => 4
-'''
+"""
 
 import sys
+
 input = sys.stdin.readline
 n, m = map(int, input().split())
 A = [[0] * (n + 1)]
@@ -29,9 +30,9 @@ for i in range(n):
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
+        D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+    result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1]
     print(result)
